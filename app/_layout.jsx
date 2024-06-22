848175e6431570a4e6874349c7453b875fda6e87
Stephen Muli <ascorpiamuli@gmail.com>
2024-06-22 10:49:24 +0300

    pages

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
 import { SplashScreen, Stack } from "expo-router";
+import GlobalProvider from "../context/GlobalProvider";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -31,9 +32,16 @@ const RootLayout = () => {
     return null;
   }
 
-  if (!fontsLoaded && !error) {
-    return null;
-  }
+  return (
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
+  );
 };
 
 export default RootLayout;
